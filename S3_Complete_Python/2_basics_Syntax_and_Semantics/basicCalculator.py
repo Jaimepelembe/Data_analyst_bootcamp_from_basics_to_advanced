48d3f23043120406ea0c7aa17d78bbd0d973a21a
Jaimepelembe <jaimitopele@gmail.com>
2025-12-15 19:28:43 +0200

    Added the file of Operators in python and added the basic operations of floor division, modulos and exponentiation.

---
+++ S3_Complete_Python/2_basics_Syntax_and_Semantics/basicCalculator.py
@@ -0,0 +1,56 @@
+#Basic calculator
+while True:
+    inputString=input("Insert number one?")
+    numberOne=float(inputString)
+    inputString=input("Insert number two?")
+    numberTwo=float(inputString)
+
+    print("Choose the operation: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5.Floor division \n 6. Modulus \n 7. Exponentiation ")
+    inputString=input()
+    operation=int(inputString)
+
+    match operation:
+        case 1:
+            operationResult= numberOne+numberTwo
+            print(operationResult)
+
+        case 2:
+            operationResult=numberOne-numberTwo
+            print(operationResult)
+        case 3:
+            operationResult=numberOne*numberTwo
+            print(operationResult)
+    
+        case 4:
+            if numberTwo !=0:
+                operationResult=numberOne/numberTwo
+                print(operationResult)
+            else:
+                print("The denominator is 0. And it is impossible to divide by zero")
+            
+        case 5:
+            if numberTwo !=0:
+                operationResult=numberOne//numberTwo
+                print(operationResult)
+            else:
+                print("The denominator is 0. And it is impossible to divide by zero")
+        
+        case 6:
+            if numberTwo !=0:
+                operationResult=numberOne%numberTwo
+                print(operationResult)
+            else:
+                print("The denominator is 0. And it is impossible to divide by zero")
+        
+        case 7:
+            if numberOne !=0 and numberTwo>=0:
+                operationResult=numberOne**numberTwo
+                print(operationResult)
+            else:
+                print("The denominator is 0. And it is impossible to divide by zero")
+            
+    print("Do you want to continue? \n 1.Yes \n 2. No")
+    inputString=input()
+    decision=int(inputString)
+    if decision==2:
+        break
