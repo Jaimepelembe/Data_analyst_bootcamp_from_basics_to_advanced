88755cc87341d794d0b36564ba500a63b76e2ccb
Jaimepelembe <jaimitopele@gmail.com>
2026-02-24 13:21:51 +0200

    Feat: Modified the files assignmentAthletes.ipynb, datasetAutiliarFunctions.py and added the file datasetAuxiliarFunctions.cpython-313.pyc

--- S4_Data_Analysis/5_matplotlib/packages/datasetAuxiliarFunctions.py
+++ S4_Data_Analysis/5_matplotlib/packages/datasetAuxiliarFunctions.py
@@ -33,6 +33,8 @@ def removeOutliers(dataset,column):
     Returns the dataset without outliers in the column
     """
     try:
+
+        import numpy as np
         q1,q3 = np.percentile(dataset[column], [25,75])
         print(f" Q1 and Q3 = {q1} : {q3}" )
         
@@ -58,4 +60,33 @@ def removeOutliers(dataset,column):
 #removeOutliers(athletesDataset,"Weight")
 
 #athletesDataset
+
+
+def fillColumnMean(dataset,column,dtype=int):
+    """Fill the NaN values of the Dataset with the mean value of the column.
     
+    Parameters:
+
+        dataset: The dataset.
+        column: The column.
+        dtype(int or float): The data type of the column.
+
+    Returns:
+        Dataset.
+    """
+
+
+    try:
+        dataset[column]=dataset[column].fillna(dtype(dataset[column].mean()))
+    
+    except KeyError as key:
+        print(f"The column {column} doesn't exist in the dataset.")
+    
+    except NameError as ne:
+        print(f"The dataset {dataset} doesn't exist.")
+
+    except Exception as ex:
+        print(ex)
+    
+    else:
+        return dataset
